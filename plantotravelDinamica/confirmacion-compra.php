7904c84479b15c74872941b46632a1ef55b3a0bc
javimv36 <javimv36@gmail.com>
2017-04-27 13:30:22 +0200

    Iniciando web dinamica

---
+++ plantotravelDinamica/confirmacion-compra.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <title>PlanToTravel</title>
+    <link id="estiloComprartido" rel="stylesheet" type="text/css" href="distribucion.css" />
+
+    <link id="estiloCabecera" rel="stylesheet" type="text/css" href="cabecera.css" />
+    <link href="js/vis-timeline-graph2d.min.css" rel="stylesheet" type="text/css" />
+    <script src="js/vis.js"></script>
+    <link rel="stylesheet" type="text/css" href="confirmacion-compra.css" />
+    <link rel="stylesheet" type="text/css" href="timeline.css" />
+    <meta charset="utf-8">
+</head>
+
+<body>
+  <?php
+    include("cabecera.php");
+    include("sidebarIzq.php");
+    include("compraExcursiones.php");
+    include("footer.php");
+  ?>
+</body>
+
+</html>
---
+++ plantotravelDinamica/sidebarIzq.php
@@ -0,0 +1,8 @@
+<div class="sidebar">
+    <div class="sidebarContainer">
+        <div class="bloqueSidebar">
+            <h3> Información de compra </h3>
+            <p>En esta página se muestra un recibo con los datos de la compra y el precio de cada uno así como el total del coste de la compra y un formulario de pago a continuación para realizar el pago que redireccionará a una página de éxito.</p>
+        </div>
+    </div>
+</div>
